ef8ee4dd80720f6fca808827ff8267756eaffe98
Andrey Frolov <a.frolov@sftpro.ru>
2023-07-23 22:05:17 +0300

    add webapi request feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_224125) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_150853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_224125) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "parent_id"
+    t.string "cat_id"
     t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
@@ -53,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_224125) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "sku"
+    t.jsonb "webapi_data", default: {}
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
