9d2d51442fb10a6a1d076bb7ca738ae93c8eb8b0
Andrey Frolov <a.frolov@sftpro.ru>
2023-07-27 21:03:15 +0300

    add less price  filter

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  SUB_CATEGORIES = { women: 'Женщины', home: 'Дом', kitchen: 'Кухня' }
+
   has_many :subcategories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
   belongs_to :parent, class_name: "Category", foreign_key: "parent_id", optional: true
 
@@ -7,5 +9,7 @@ class Category < ApplicationRecord
   scope :with_subcategories, -> { where.not(parent_id: nil) }
   scope :parents, -> { where(parent_id: nil) }
 
-  scope :women, -> { find(21).subcategories.pluck(:name) }
+  scope :women, -> { find_by(name: SUB_CATEGORIES[:women]).subcategories.pluck(:name) }
+  scope :home, -> { find_by(name: SUB_CATEGORIES[:home]).subcategories.pluck(:name) }
+  scope :kitchen, -> { find_by(name: SUB_CATEGORIES[:home]).subcategories.pluck(:name) }
 end
