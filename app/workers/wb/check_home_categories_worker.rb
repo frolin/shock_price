449b51e1855173c5e7f0c315c0a6d4697ccff5c0
Andrey Frolov <a.frolov@sftpro.ru>
2023-07-30 01:10:48 +0300

    add root kitchen category

--- app/workers/wb/check_home_categories_worker.rb
+++ app/workers/wb/check_home_categories_worker.rb
@@ -6,6 +6,8 @@ module Wb
       Keyword.pluck(:name).each do |name|
         ::Wb::CheckDiscountsWorker.perform_async(name, [1], 50)
       end
+
+       ::Wb::CheckDiscountsWorker.perform_async('Кухня', [1,2,3,4,5], 50)
     end
   end
 end
