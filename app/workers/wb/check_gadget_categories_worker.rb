8fb6751064501f6941dda9a0eb7907a0e14986b2
Andrey Frolov <a.frolov@sftpro.ru>
2023-08-11 11:43:08 +0300

    change sale price

--- app/workers/wb/check_gadget_categories_worker.rb
+++ app/workers/wb/check_gadget_categories_worker.rb
@@ -4,7 +4,7 @@ module Wb
 
     def perform
       Category.gadgets.find_each do |category|
-        ::Wb::CheckDiscountsWorker.perform_async(category.name, 'гаджеты', [1,2,3,4,5], 100)
+        ::Wb::CheckDiscountsWorker.perform_async(category.name, 'гаджеты', [1,2,3,4,5], 30)
       end
 
     end
