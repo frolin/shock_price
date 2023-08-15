0bb2b7e6e801b24b513dd78902340fdf1e5a90e5
andrey <fralunia@gmail.com>
2023-08-15 20:51:53 +0300

    feedbacks filter

--- app/processes/wb/discounts_check.rb
+++ app/processes/wb/discounts_check.rb
@@ -152,11 +152,11 @@ module Wb
       text << "\n"
       text << "🏘 Акция <b>#{format_price(product_data[:sale_name])}</b> \n \n" if product_data[:sale_name]
 
-      text << "💰Цена <b>#{format_price(product_data[:new_price])}</b> ❗<s>#{format_price(product_data[:old_price])}</s> ️ \n"
-      text << "🔴 #{product_data[:sells_count]} \n" if product_data[:sells_count]&.to_i > 50
+      text << "💰Цена <b>#{format_price(product_data[:new_price])}</b> ❗<s>#{format_price(product_data[:old_price])}</s> ️\n"
+      # text << "🔴 #{product_data[:sells_count]} \n" if product_data[:sells_count]&.to_i > 50
       # text << "📈 <b>История цены: </b>#{product_data[:price_history]}₽ \n\n" if product_data[:price_history]
       # text << "🏷 <b>Категория: </b> #{product_data[:subject]} \n"
-      text << "🆔 <b>Артикул: </b> `#{product_data[:sku]}`\n"
+      text << "\n🆔 <b>Артикул: </b> `#{product_data[:sku]}"
       text << "\n #{product_data[:name]} \n"
 
       text << "\n 👉 <a href='#{product_data[:link]}'>Товар на Wildberries</a> 👈 \n"
