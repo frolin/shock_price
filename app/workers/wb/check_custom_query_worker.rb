cc39b5e49956877c1ad8f3dd986da9d7c848e2e9
andrey <fralunia@gmail.com>
2023-09-26 12:34:59 +0300

    add custom queries fix

--- app/workers/wb/check_custom_query_worker.rb
+++ app/workers/wb/check_custom_query_worker.rb
@@ -1,18 +1,21 @@
 module Wb
   class CheckCustomQueryWorker
     include Sidekiq::Worker
-    
+
     def perform
       cosmetics = ['Бальзам для губ', 'Бальзам для губ', 'Твердый бальзам для волос', 'Крем для рук и тела', 'Твердое масло для тела' ]
       clothes = ['Шуба женская', 'Шуба зимняя искусственна', 'Шапка бини', 'Свитер вязаный оверсайз']
       gifts = ['Открытки набор', 'Фотофон Газета', 'стильные картины по номерам', 'Подсвечник декоративный', 'Наволочка декоративная', 'Свеча ароматическая в банке', 'Брелок для ключей женский', 'Журнал Seasons of life', 'Журналы']
+
+
+      queries = cosmetics +  clothes + gifts
       
       queries.each do |name|
         ::Wb::CheckDiscountsWorker.perform_async(name, 'женщины', [1], 30)
       end
 
        ::Wb::CheckDiscountsWorker.perform_async('Товары для уборки', 'женщины', [1,2,3,4,5], 50)
-       ::Wb::CheckDiscountsWorker.perform_async('Маникюр', 'женщины', [1,2,3], 30)
+       ::Wb::CheckDiscountsWorker.perform_async('Маникюр', 'женщины', [1,2,3,4,5], 30)
     end
   end
 end
