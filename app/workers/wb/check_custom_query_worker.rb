513a6b28eb5774f29348918d23824f4ce8ad17e1
andrey <fralunia@gmail.com>
2023-09-26 11:45:09 +0300

    add custom queries

---
+++ app/workers/wb/check_custom_query_worker.rb
@@ -0,0 +1,20 @@
+module Wb
+  class CheckCustomQueryWorker
+    include Sidekiq::Worker
+
+    sidekiq_options queue: :check_discounts, retry: 3
+    
+    def perform
+      cosmetics = ['Бальзам для губ', 'Бальзам для губ', 'Твердый бальзам для волос', 'Крем для рук и тела', 'Твердое масло для тела' ]
+      clothes = ['Шуба женская', 'Шуба зимняя искусственна', 'Шапка бини', 'Свитер вязаный оверсайз']
+      gifts = ['Открытки набор', 'Фотофон Газета', 'стильные картины по номерам', 'Подсвечник декоративный', 'Наволочка декоративная', 'Свеча ароматическая в банке', 'Брелок для ключей женский', 'Журнал Seasons of life', 'Журналы']
+      
+      queries.each do |name|
+        ::Wb::CheckDiscountsWorker.perform_async(name, 'женщины', [1], 30)
+      end
+
+       ::Wb::CheckDiscountsWorker.perform_async('Товары для уборки', 'женщины', [1,2,3,4,5], 50)
+       ::Wb::CheckDiscountsWorker.perform_async('Маникюр', 'женщины', [1,2,3,4,5], 30)
+    end
+  end
+end
